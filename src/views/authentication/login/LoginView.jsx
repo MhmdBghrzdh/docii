900789a1960f00dba3687656c105ec611bdcc7df
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-02-03 01:23:51 +0330

    feat : loading-spinner

--- src/views/authentication/login/LoginView.jsx
+++ src/views/authentication/login/LoginView.jsx
@@ -1,5 +1,12 @@
+import BaseButton from '@/components/base/base-button/BaseButton'
+
 function LoginView() {
-  return <h1>login</h1>
+  return (
+    <>
+      <h1>login</h1>
+      <BaseButton text="Booking" isLoading={false} />
+    </>
+  )
 }
 
 export default LoginView
