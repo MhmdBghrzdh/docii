b927521df2106c7a13e418fd21b1be27233141fc
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-02-03 01:22:56 +0330

    feat : base-button

--- src/components/base/base-button/BaseButton.jsx
+++ src/components/base/base-button/BaseButton.jsx
@@ -1,5 +1,24 @@
-function BaseButton() {
-  return;
+import LoadingSpinner from '@/components/general/loading-spinner/LoadingSpinner'
+
+import PropTypes from 'prop-types'
+import './index.scss'
+
+function BaseButton({ text, variant = 'filled', isBlock = true, isLoading = true }) {
+  const buttonBaseClass = 'base-button'
+  const buttonVariant = `base-button_${variant}`
+  const buttonIsBlock = isBlock ? 'base-button_block' : ''
+
+  return (
+    <button className={`${buttonBaseClass} ${buttonVariant} ${buttonIsBlock}`}>
+      {isLoading ? <LoadingSpinner /> : text}
+    </button>
+  )
 }
 
-export default BaseButton;
+export default BaseButton
+BaseButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(['filled', 'text', 'outlined']),
+  isBlock: PropTypes.bool,
+  isLoading: PropTypes.bool
+}
