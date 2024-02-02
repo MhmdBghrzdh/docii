4b3ea181b4ad58edf0186bc3863ecdda7f555a5e
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-02-02 23:47:32 +0330

    feat : define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import LoginView from '@/views/authentication/login/LoginView.jsx'
+import SignupView from '@/views/authentication/signup/SignupView.jsx'
+import ForgotPasswordView from '@/views/authentication/forgot-password/ForgotPasswordView.jsx'
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/signup" element={<SignupView />} />
+        <Route path="/forgot-password" element={<ForgotPasswordView />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
