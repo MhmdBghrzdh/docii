e2303d22f1f3790ead093562aa015be2bb175752
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-02-22 17:55:29 +0330

    feat: validation

--- src/mappers/authentication/index.js
+++ src/mappers/authentication/index.js
@@ -9,3 +9,5 @@ export const signupMapper = ({ nationalCode, firstName, lastName, password, phon
   password,
   mobileNumber: phoneNumber
 })
+
+export const loginMapper = ({ nationalCode, password }) => ({ password, nationalId: nationalCode })
