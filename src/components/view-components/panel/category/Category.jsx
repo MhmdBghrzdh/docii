4b607ac2bdbbbae2b17fcba953764f7d12df5a17
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-01 23:04:57 +0330

    fix : use base icon in category.jsx

--- src/components/view-components/panel/category/Category.jsx
+++ src/components/view-components/panel/category/Category.jsx
@@ -1,26 +1,22 @@
 import style from './index.module.scss'
 
 import PropTypes from 'prop-types'
-// import BaseIcon from '@/components/base/base-icon/BaseIcon'
+import BaseIcon from '@/components/base/base-icon/BaseIcon'
 
-function Category({ title, image, isLoading }) {
-  const imageSource = image ? `data:image/png;base64,${image}` : null
+function Category({ title, image }) {
   return (
-    !isLoading && (
-      <div className={style['category']}>
-        <div className={style['category__icon-wrapper']}>
-          <img src={imageSource} alt={title} />
-        </div>
-        <h3 className={style['category__title']}>{title}</h3>
+    <div className={style['category']}>
+      <div className={style['category__icon-wrapper']}>
+        <BaseIcon name={image} />
       </div>
-    )
+      <h3 className={style['category__title']}>{title}</h3>
+    </div>
   )
 }
 
 Category.propTypes = {
   image: PropTypes.string,
-  title: PropTypes.string,
-  isLoading: PropTypes.bool
+  title: PropTypes.string
 }
 
 export default Category
