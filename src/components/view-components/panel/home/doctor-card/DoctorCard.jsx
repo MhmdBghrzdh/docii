e9c5afb60a9629fcece5d063944ea4c5afcab904
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-01 23:24:07 +0330

    feat : imageSource in doctor card

--- src/components/view-components/panel/home/doctor-card/DoctorCard.jsx
+++ src/components/view-components/panel/home/doctor-card/DoctorCard.jsx
@@ -4,20 +4,27 @@ import PropTypes from 'prop-types'
 
 import CheapScore from '@/components/general/cheap-score/CheapScore'
 
-function DoctorCard({ name, image, category, score }) {
+function DoctorCard({ firstName, lastName, image, category, score }) {
+  const imageSource = image
+    ? `data:image/png;base64,${image}`
+    : 'src/assets/images/doctorDefaultPerson.png'
+
   return (
     <div className={styles['doctor-card']}>
-      <img className={styles["doctor-card__image"]} src={image} alt={name} />
-      <h3 className={styles['doctor-card__name']}>{name}</h3>
+      <img className={styles['doctor-card__image']} src={imageSource} alt={firstName + lastName} />
+      <h3 className={styles['doctor-card__name']}>
+        {firstName} {lastName}
+      </h3>
       <span className={styles['doctor-card__category']}>{category}</span>
       <CheapScore score={score} />
-    </div>  
+    </div>
   )
 }
 
 DoctorCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  image: PropTypes.string,
   category: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired
 }
