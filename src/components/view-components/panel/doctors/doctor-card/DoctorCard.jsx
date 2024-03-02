affecb08b275bad910d818eec3732a62b5b1319d
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-02 23:07:44 +0330

    feat : navigate in doctor card

--- src/components/view-components/panel/doctors/doctor-card/DoctorCard.jsx
+++ src/components/view-components/panel/doctors/doctor-card/DoctorCard.jsx
@@ -1,17 +1,24 @@
 import styles from './index.module.scss'
 
 import PropTypes from 'prop-types'
+import { useNavigate } from 'react-router-dom'
 
 import CheapScore from '@/components/general/cheap-score/CheapScore'
 import BaseIcon from '@/components/base/base-icon/BaseIcon'
 
-function DoctorCard({ firstName, lastName, image, category, score }) {
+function DoctorCard({ firstName, lastName, image, category, score, id }) {
   const imageSource = image
     ? `data:image/png;base64,${image}`
     : 'src/assets/images/doctorDefaultPerson.png'
 
+  const navigate = useNavigate() 
+
+  const navigateToDoctorPage = (doctorId) => {
+    console.log(doctorId)
+    navigate(`/doctor/${doctorId}`)
+  }
   return (
-    <div className={styles['doctor-card']}>
+    <div className={styles['doctor-card']} onClick={navigateToDoctorPage(id)}>
       <img className={styles['doctor-card__image']} src={imageSource} alt={firstName + lastName} />
       <div className={styles['doctor-card__details']}>
         <h3 className={styles['doctor-card__name']}>
@@ -28,6 +35,7 @@ DoctorCard.propTypes = {
   firstName: PropTypes.string.isRequired,
   lastName: PropTypes.string.isRequired,
   image: PropTypes.string,
+  id: PropTypes.string,
   category: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired
 }
