61b802c0e99e5d2a6f76e194199d4d05fcc9ad77
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-15 17:35:28 +0330

    feat : change login route

--- src/constants/common/panel/shared/nav-list.constants.js
+++ src/constants/common/panel/shared/nav-list.constants.js
@@ -2,5 +2,5 @@ export const NAV_LIST = [
   { path: '/', name: 'Home' },
   { path: '/messages', name: 'Message' },
   { path: '/schedule', name: 'Calendar' },
-  { path: '/profile', name: 'Profile' }
+  { path: '/login', name: 'Profile' }
 ]
