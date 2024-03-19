d27bcbc3614d02800ccac127c38b82284c3db3ef
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-19 16:43:21 +0330

    style : present time

--- src/components/general/present-time/PresentTime.jsx
+++ src/components/general/present-time/PresentTime.jsx
@@ -2,17 +2,21 @@ import style from './index.module.scss'
 
 import PropTypes from 'prop-types'
 
-function PresentTime({ time, isActive }) {
+function PresentTime({ time, isActive, reserved, className = '', onClick }) {
+  const combinedClasses = `${style['present-time']} ${className} ${isActive ? style['present-time_active'] : ''} ${reserved ? style['presentTime_disabled'] : ''}`
+
   return (
-    <span className={`${style['present-time']} ${isActive ? style['present-time_active'] : ''}`}>
+    <button className={combinedClasses} onClick={onClick} disabled={reserved}>
       {time}
-    </span>
+    </button>
   )
 }
 
 PresentTime.propTypes = {
   time: PropTypes.string.isRequired,
   reserved: PropTypes.number.isRequired,
-  isActive: PropTypes.bool
+  isActive: PropTypes.bool,
+  className: PropTypes.string,
+  onClick: PropTypes.func
 }
 export default PresentTime
