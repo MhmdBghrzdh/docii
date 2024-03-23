879ce9d8e90598e73c9ba75c1a3bdb3d1a1563aa
mohammad bagherzadeh <mohammadbagherzadeh1380@gmail.com>
2024-03-23 15:35:27 +0330

    feat: schedule view

--- src/views/panel/schedules/ScheduleView.jsx
+++ src/views/panel/schedules/ScheduleView.jsx
@@ -1,7 +1,19 @@
-import './index.module.scss'
+import styles from './index.module.scss'
+import { SCHEDULE_TABS } from '@/constants/common/panel/schedules/schedule-tabs.constants.js'
+import BaseTabs from '@/components/base/base-tabs/BaseTabs.jsx'
+import { useState } from 'react'
 
 function SchedulesView() {
-  return <div>Schedules</div>
+  const [activeScheduleTab, setActiveScheduleTab] = useState(0)
+  const handleTabClick = (index) => {
+    setActiveScheduleTab(index)
+  }
+  return <div className={styles['schedules-view']}>
+
+    <BaseTabs tabs={SCHEDULE_TABS} activeTab={activeScheduleTab} onClick={(index) => handleTabClick(index)}
+              className={styles['schedule-view__schedule-tabs']} />
+
+  </div>
 }
 
 export default SchedulesView
